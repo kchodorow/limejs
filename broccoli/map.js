96b602118945bae4b67090c439e71d148312114a
Kristina <kristina@10gen.com>
2014-05-04 12:39:01 -0400

    Remove square & acre classes

--- broccoli/map.js
+++ broccoli/map.js
@@ -23,27 +23,14 @@ broccoli.Map.prototype.has_acre = function(acre_x, acre_y) {
 broccoli.Map.prototype.acre = function(acre_x, acre_y) {
     var acre = this.acre_.get(acre_x, acre_y);
     if (!acre) {
-        acre = new broccoli.Acre();
         this.forest_ranger_.generate(
             acre_x * broccoli.Acre.WIDTH, acre_y * broccoli.Acre.HEIGHT);
-        this.acre_.set(acre_x, acre_y, acre);
+        this.acre_.set(acre_x, acre_y, true);
     }
     return acre;
 };
 
 // Map sections: allocated in 20x10 arrays of squares.
-broccoli.Acre = function() {
-    this.square_ = new Array(broccoli.Acre.WIDTH);
-    for (var i = 0; i < broccoli.Acre.WIDTH; ++i) {
-        this.square_[i] = new Array(broccoli.Acre.HEIGHT);
-        for (var j = 0; j < broccoli.Acre.HEIGHT; ++j) {
-            this.square_[i][j] = new broccoli.Square();
-        }
-    }
-};
-
+broccoli.Acre = {};
 broccoli.Acre.WIDTH = 20;
 broccoli.Acre.HEIGHT = 10;
-
-broccoli.Square = function() {
-};
